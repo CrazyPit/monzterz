834e580853023af4e126d1e238c9845f8bb49150
Peter Rezikov <peter.rezikov@toptal.com>
2020-09-03 19:33:56 +0300

    Updates

---
+++ precache-manifest.8efb81d8098fae061b1ad8e243fce4af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606ed714125f2764fda9306062cb67c0",
+    "url": "/monzterz/index.html"
+  },
+  {
+    "revision": "7db65e6d434369ee0578",
+    "url": "/monzterz/static/css/main.6752228b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monzterz/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monzterz/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7db65e6d434369ee0578",
+    "url": "/monzterz/static/js/main.663feca4.chunk.js"
+  },
+  {
+    "revision": "5ecf5df51adbf9771426",
+    "url": "/monzterz/static/js/runtime-main.324f01cc.js"
+  }
+]);
